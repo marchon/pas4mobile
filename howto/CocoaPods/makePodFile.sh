2aec23db5342485f87f0c7427fb351b22586cc1b
George Lambert <marchon@Georges-MacBook-Pro.local>
2014-08-05 23:44:51 -0400

    updates

--- howto/CocoaPods/makePodFile.sh
+++ howto/CocoaPods/makePodFile.sh
@@ -1,3 +1,3 @@
-echo pod 'StompKit', :git => 'https://github.com/mobile-web-messaging/StompKit.git' > Podfile
+echo "pod 'StompKit', :git => 'https://github.com/mobile-web-messaging/StompKit.git'" > Podfile
 pod install
 
